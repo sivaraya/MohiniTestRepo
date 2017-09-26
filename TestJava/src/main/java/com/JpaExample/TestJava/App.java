ce482810a6df364500bc1775d5455162ea92a3d4
Mohini <Mohini@mbaniset.in.ibm.com>
2017-09-26 18:26:49 +0530

    Commited Test project

---
+++ TestJava/src/main/java/com/JpaExample/TestJava/App.java
@@ -0,0 +1,13 @@
+package com.JpaExample.TestJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
